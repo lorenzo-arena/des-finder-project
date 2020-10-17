d7e7b4c18ff38a197e664900fb45841f8ea70a9f
Lorenzo Arena <arena.lor@gmail.com>
2020-10-17 14:14:22 +0200

    [ADD] Added command line option parsing

--- src/sequential/main.c
+++ src/sequential/main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <argp.h>
 
 #define _GNU_SOURCE         /* See feature_test_macros(7) */
 #include <crypt.h>
@@ -8,25 +9,76 @@
 #include "log.h"
 #include "defines.h"
 
-#define SALT "sa"
+const char *argp_program_version =
+" 1.0";
 
-#define PWD "testtest"
+struct arguments
+{
+    char *args[2];                /* hash and salt */
+    char *dictionary_path;        /* Argument for -d */
+};
 
-int main() {
-    struct timespec start, end;
-    struct crypt_data data = {
-        .input = PWD,
-        .initialized = 0
-    };
+void set_default_arguments(struct arguments *arguments)
+{
+    arguments->args[0] = "";
+    arguments->args[1] = "";
+    arguments->dictionary_path = DICTIONARY_FILENAME;
+}
 
-    printf("Hello!\n");
+static struct argp_option options[] =
+{
+    {"dictionary", 'd', DICTIONARY_FILENAME, 0, "Set the dictionary file path", 0}
+};
 
-    clock_gettime(CLOCK_MONOTONIC, &start);
-    crypt_r(PWD, SALT, &data);
-    clock_gettime(CLOCK_MONOTONIC, &end);
+static error_t parse_opt (int key, char *arg, struct argp_state *state)
+{
+    struct arguments *arguments = state->input;
 
-    printf("Crypted pwd: %s\n", data.output);
-    printf("Elapsed %ldns\n", end.tv_nsec - start.tv_nsec);
+    switch (key)
+    {
+        case 'd':
+            arguments->dictionary_path = arg;
+            break;
+        case ARGP_KEY_ARG:
+            if (state->arg_num >= 2)
+            {
+                argp_usage(state);
+            }
+            arguments->args[state->arg_num] = arg;
+            break;
+        case ARGP_KEY_END:
+            if (state->arg_num < 2)
+            {
+                argp_usage(state);
+            }
+            break;
+        default:
+        return ARGP_ERR_UNKNOWN;
+    }
+
+    return 0;
+}
+
+static char args_doc[] = "hash salt";
+
+static char doc[] =
+"des-finder-seq -- Used to find passwords from a dictionary and an hash and salt.";
+
+static struct argp argp = {options, parse_opt, args_doc, doc, NULL, NULL, NULL};
+
+int main(int argc, char *argv[]) {
+    const char *hash;
+    const char *salt;
+    const char *dictionary_path;
+    struct arguments arguments;
+
+    set_default_arguments(&arguments);
+
+    argp_parse(&argp, argc, argv, 0, 0, &arguments);
+
+    hash = arguments.args[0];
+    salt = arguments.args[1];
+    dictionary_path = arguments.dictionary_path;
 
     return 0;
 }
