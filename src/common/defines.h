b1800a2d3b1866e335e27b36318b8e2400481c84
Lorenzo Arena <arena.lor@gmail.com>
2020-11-01 21:35:36 +0100

    [ADD] Added sequential dictionary generation

--- src/common/defines.h
+++ src/common/defines.h
@@ -4,7 +4,10 @@
 #define PWD_DIMENSION 8
 #define PWD_CHAR_SPACE "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
+#define HASH_DIMENSION 13
+
 #define SALT "sa"
+#define SALT_SIZE (sizeof(SALT) - 1)
 
 #define DICTIONARY_FILENAME "dictionary.txt"
 #define DICTIONARY_HASH_FILENAME "dictionary_hash.txt"
